8d6dd18846fc7c67ef3c9abd7858afd5fade5dfe
K1502679 <k1502679@kcl.ac.uk>
2016-11-24 16:45:47 +0000

    added Special Int scala file

---
+++ Special Int/SpecialInt.scala
@@ -0,0 +1,49 @@
+import scala.language.postfixOps //otherwise it will throw warning when trying to do num++
+
+type int = SpecialInt
+
+class SpecialInt(value: Int) {
+
+  var mValue = value
+
+  //post-increment
+  def ++(): int = {
+
+    val toReturn = new int(mValue)
+    mValue += 1
+    return toReturn
+  }
+
+  //post-decrement
+  def --(): int = {
+
+    val toReturn = new int(mValue)
+    mValue -= 1
+    return toReturn
+  }
+
+  //a readable toString
+  override def toString(): String = {
+    return mValue.toString
+  }
+}
+
+object SpecialInt {
+
+  //pre-increment
+  def ++(n: int): int = {
+    n.mValue += 1
+    return n;
+  }
+
+  //pre-decrement
+  def --(n: int): int = {
+    n.mValue -= 1
+    return n;
+  }
+
+  //Something to get normal Int
+  def *(n: int): Int = {
+    return n.mValue
+  }
+}
